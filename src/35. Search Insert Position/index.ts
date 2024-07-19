a384ae4e98136e4faf8712f134e4adcdf1728d06
mrnobody1384 <ostoore69@gmail.com>
2024-07-19 15:33:05 +0330

    Question 35 Solved & Ready For Next.

---
+++ src/35. Search Insert Position/index.ts
@@ -0,0 +1,17 @@
+function searchInsert(nums: number[], target: number): number {
+    for (let i = nums.length; i >= 0 ; i--) {
+        let after = nums[i - 1];
+        let current = nums[i];
+        let lastIndex = nums.length -1
+        let last = nums[lastIndex]
+        if (current == target) return i;
+        if (target < current && target > after)return i;
+        if (target > last) return lastIndex + 1;
+    }
+    
+    return 0
+};
+
+const nums__ = [1,3,5,6]
+const target = 5
+console.log(searchInsert(nums__,target))
