9df4a045318bfe7dfe00fc8d644274f9671ee573
mrnobody1384 <ostoore69@gmail.com>
2024-07-23 14:34:43 +0330

    Prepare Code But Not Solved Yet.

---
+++ dist/70. Climbing Stairs/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function climbStairs(n) {
+    let result = 0;
+    return result;
+}
+;
+//# sourceMappingURL=index.js.map
---
+++ src/70. Climbing Stairs/index.ts
@@ -0,0 +1,4 @@
+function climbStairs(n: number): number {
+    let result = 0
+    return result
+};
