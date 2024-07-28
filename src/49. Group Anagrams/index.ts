6caf65a16a158fd69af838399c16411233d621ba
mrnobody1384 <ostoore69@gmail.com>
2024-07-28 18:00:28 +0330

    Question 49 Solved & Ready For Next.

---
+++ src/49. Group Anagrams/index.ts
@@ -0,0 +1,33 @@
+function groupAnagrams(strs: string[]): string[][] {
+    console.log(strs.length)
+    const newArr: string[][] = []
+    if (strs.length == 1){
+        newArr.push(strs)
+        return newArr  
+    }
+    let sortString = (stringg:string) => {
+        return stringg.split("").sort().join("");
+    };
+
+    const mmap = new Map()
+    for (let str of strs) {
+       let sorted= sortString(str)
+        if (!mmap.has(sorted)){
+           mmap.set(sorted,[str])
+        }
+        else{
+            mmap.set(sorted,[...mmap.get(sorted),str])
+        }
+    }
+    for (let i of mmap.values()){
+        newArr.push(i)
+    }
+    return newArr
+};
+
+//expected = [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+
+let strs = ["eat","tea","tan","ate","nat","bat"]
+
+console.log(groupAnagrams(strs))
