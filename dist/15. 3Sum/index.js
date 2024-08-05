57f40d086c40d3956ffa86c0ae82890e445b0aa9
mrnobody1384 <ostoore69@gmail.com>
2024-08-05 19:40:06 +0330

    Question 15 Ready To Find Solution.

---
+++ dist/15. 3Sum/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function threeSum(nums) {
+    return [];
+}
+let numas = [-1, 0, 1, 2, -1, -4];
+console.log(threeSum(numas));
+//# sourceMappingURL=index.js.map
---
+++ src/15. 3Sum/index.ts
@@ -0,0 +1,7 @@
+function threeSum(nums: number[]): number[][] {
+  return [];
+}
+
+let numas = [-1, 0, 1, 2, -1, -4];
+
+console.log(threeSum(numas));
