81e168a16f27d6bbacf10473297d508f4dd96a8a
mrnobody1384 <ostoore69@gmail.com>
2024-08-05 18:55:05 +0330

    Question 167 Solved & Ready For Next.

---
+++ dist/167. Two Sum II - Input Array Is Sorted/index.js
@@ -0,0 +1,25 @@
+"use strict";
+//@ts-ignore
+function twoSum(numbers, target) {
+    //   let indices: number[] = [];
+    let low = 0;
+    let high = numbers.length - 1;
+    while (low < high) {
+        let sum = numbers[low] + numbers[high];
+        if (sum == target) {
+            return [low + 1, high + 1];
+        }
+        if (sum < target) {
+            low++;
+        }
+        if (sum > target) {
+            high--;
+        }
+    }
+}
+let numbers = [2, 7, 11, 15], targett = 9;
+// numbers = [0, 0, 3, 4];
+// targett = 0;
+// numbers = [5, 25, 75];
+console.log(twoSum(numbers, targett));
+//# sourceMappingURL=index.js.map
